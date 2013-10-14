afddc1b2d664bb0c26b18b7baeb7d6323cc1271a
MakerSquare Student <students@themakersquare.com>
2013-10-14 10:33:42 -0500

    Add Devise layout page

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 20131011204136) do
-=======
 ActiveRecord::Schema.define(version: 20131014151232) do
->>>>>>> Stashed changes
 
   create_table "phases", force: true do |t|
     t.string   "name"
@@ -48,19 +44,10 @@ ActiveRecord::Schema.define(version: 20131014151232) do
   add_index "projects", ["phase_id"], name: "index_projects_on_phase_id"
 
   create_table "users", force: true do |t|
-<<<<<<< Updated upstream
-    t.string   "password"
     t.text     "profile_pic"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
     t.string   "name"
-=======
-    t.string   "first_name"
-    t.string   "user_name"
-    t.text     "profile_pic"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -71,7 +58,6 @@ ActiveRecord::Schema.define(version: 20131014151232) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
->>>>>>> Stashed changes
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
