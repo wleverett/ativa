3d736dcc527e3d7b086a078921318a0a688dc065
MakerSquare Student <students@themakersquare.com>
2013-10-14 10:16:51 -0500

    merging in changes from origin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 20131011204136) do
+=======
+ActiveRecord::Schema.define(version: 20131014151232) do
+>>>>>>> Stashed changes
 
   create_table "phases", force: true do |t|
     t.string   "name"
@@ -44,12 +48,33 @@ ActiveRecord::Schema.define(version: 20131011204136) do
   add_index "projects", ["phase_id"], name: "index_projects_on_phase_id"
 
   create_table "users", force: true do |t|
+<<<<<<< Updated upstream
     t.string   "password"
     t.text     "profile_pic"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
     t.string   "name"
+=======
+    t.string   "first_name"
+    t.string   "user_name"
+    t.text     "profile_pic"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+>>>>>>> Stashed changes
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
